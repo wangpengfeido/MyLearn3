fe04ad15d6fe5f41827bd33e2749dec8015a1ffa
wangpengfeido <wangpengfeido@126.com>
2018-09-13 23:57:13 +0800

    ...

---
+++ LearnRabitMQ/010tutorials/010start/consumer/receive.js
@@ -0,0 +1,15 @@
+const amqp = require('amqplib/callback_api')
+
+amqp.connect('amqp://localhost:5672', function (err, conn) {
+    conn.createChannel(function (err, ch) {
+        // 与生产者一样，连接rabbitMQ服务，创建channel，声明队列
+        const q = 'hello'
+        // 声明队列的原因是消费者有可能先于生产者运行，保证队列存在
+        ch.assertQueue(q, {durable: false})
+        console.log(`[*] Wating for messages in ${q}.To exit press CTRL+C`)
+        ch.consume(q, function (msg) {
+            // 接收消息并异步处理。此处接收的消息队列与使用sendToQueue发送的对应。
+            console.log(`[x] Received ${msg.content.toString()}`)
+        }, {noAck: true})
+    })
+})
--- LearnRabitMQ/RabbitMQ安装使用总结.md
+++ LearnRabitMQ/RabbitMQ安装使用总结.md
@@ -1,14 +1,22 @@
 ##RabbitMQ安装
 1. 安装erlang。一种编程语言。
-2. 安装RabbitMQ。默认端口4369。
+2. 安装RabbitMQ。默认端口5672。
 ##命令
 windows中命令脚本文件的位置：RabbitMQ Server\rabbitmq_server-3.7.2\sbin
-###### 查看状态
+##### 查看状态
 rabbitmqctl status
-###### 查看正在运行的队列
+##### 查看正在运行的队列
 rabbitmqctl list_queues
 * name message_ready 打印出队列中消息数量
 * name message_unacknowledged 打印出未确认的消息数量
+##### 查看已安装的插件
+rabbitmq-plugins list
+##### 启用插件
+rabbitmq-plugins enable 插件名
+## 常用插件
+##### rabbitmq_management 
+网页控制台    
+默认端口：15621；默认帐号密码guest/guest
 ##Javascript Client
 1.npm install amqplib。RabbitMQ/AMQP客户端for nodejs，支持全部协议。
 
