2f63a36273b399c868789df68dc3dac995949154
wangpengfie <wangpengfeido@126.com>
2019-07-24 19:31:35 +0800

    ...

---
+++ LearnFlutter/my_app/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flutter Demo', home: new FirstApp());
+  }
+}
+
+class FirstApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(title: new Text('welcome to flutter')),
+        body: new Center(child: new Text('hello world')));
+  }
+}
